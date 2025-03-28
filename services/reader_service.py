e239c00b9f60c5358204ef8b54c97c9027beb2dc
A'nsi Lema <ansilema@gmail.com>
2025-03-28 15:36:42 +0100

    Add filtered and general data reading scripts with queries
    Introduce `filtered_read.py` for role-based data filtering and `read_data_py.py` for general data overview. Extend `reader_service` with new query functions to support these functionalities, enabling tailored data access based on user roles.

--- services/reader_service.py
+++ services/reader_service.py
@@ -1,4 +1,6 @@
 from sqlalchemy.orm import Session
+
+from models import Collaborator
 from models.client import Client
 from models.event import Event
 from models.contract import Contract
@@ -12,3 +14,18 @@ def get_all_events(db: Session) -> list[Event]:
 
 def get_all_contracts(db: Session) -> list[Contract]:
     return db.query(Contract).all()
+
+def get_clients_by_commercial(db: Session, commercial_email: str):
+    return db.query(Client).join(Collaborator).filter(Collaborator.email == commercial_email).all()
+
+def get_contracts_not_signed(db: Session):
+    return db.query(Contract).filter_by(status=False).all()
+
+def get_contracts_not_fully_paid(db: Session):
+    return db.query(Contract).filter_by(Contract.amount_left > 0).all()
+
+def get_events_without_support(db: Session):
+    return db.query(Event).filter_by(support_id=None).all()
+
+def get_events_by_support(db: Session, support_email: str):
+    return db.query(Event).join(Collaborator).filter(Collaborator.email == support_email).all()
